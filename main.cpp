b305ec8b3ce9bf9c4876a7bcc99fbdd50b1147a9
spoooktune <asriprasana5@gmail.com>
2025-09-21 17:47:07 -0700

    Added inputData function

--- main.cpp
+++ main.cpp
@@ -15,6 +15,21 @@ struct GameLibrary{
     string *gamesList;
 };
 
+void inputData(GameLibrary *user);
+void displayData(GameLibrary *user);
+
+void inputData(GameLibrary *user){
+    cout << "Adding new user\n> Username: ";
+    cin >> user->username;
+    user->gamesList = new string[NUM_GAMES];
+    for (int i = 0; i < NUM_GAMES; i++){
+        cout << "> Game #" << i+1 << ": ";
+        getline(cin, user->gamesList[i]);
+    }
+    cin.ignore();
+    cout << endl << endl;
+}
+
 // main will contain a struct array that simulates a shared game library between friends/family
 int main(){
 
