0aee8a6f1c2afb3d6b4455c784cd5da7ac9c185f
spoooktune <asriprasana5@gmail.com>
2025-09-21 18:09:09 -0700

    Added main function, fixed formatting issues and bugs

--- main.cpp
+++ main.cpp
@@ -25,28 +25,44 @@ struct GameLibrary{
 void inputData(GameLibrary *user);
 void displayData(GameLibrary *user);
 
+
+// input data for each GameLibrary object, asks for username and the names of all games owned
 void inputData(GameLibrary *user){
     cout << "Adding new user\n> Username: ";
     cin >> user->username;
+    cin.ignore();
     user->gamesList = new string[NUM_GAMES];
     for (int i = 0; i < NUM_GAMES; i++){
         cout << "> Game #" << i+1 << ": ";
         getline(cin, user->gamesList[i]);
+        // cin.ignore();
     }
-    cin.ignore();
-    cout << endl << endl;
+    cout << endl;
 }
 
+// displays one given GameLibrary object, outputs username and full games list
 void displayData(GameLibrary *user){
     cout << "Username: " << user->username;
-    cout << "Game List: " << endl;
+    cout << "\nGame List: " << endl;
     for (int i = 0; i < NUM_GAMES; i++){
         cout << "> " << user->gamesList[i] << endl;
     }
-    cout << endl << endl;
+    cout << endl;
 }
 
-// main will contain a struct array that simulates a shared game library between friends/family
-int main(){
+/*
+ main will contain a struct array that simulates a shared game library between friends/family
+ has two for loops that first populates shared library, then outputs data for all members
+*/
+ int main(){
+    GameLibrary *sharedLib = new GameLibrary[NUM_PEOPLE];
+    for (int i = 0; i < NUM_PEOPLE; i++){
+        inputData(&sharedLib[i]);
+    }
+    cout << "\nShared Library Data\n" << endl;
+    for (int i = 0; i < NUM_PEOPLE; i++){
+        displayData(&sharedLib[i]);
+    }
 
+    return 0;
 }
